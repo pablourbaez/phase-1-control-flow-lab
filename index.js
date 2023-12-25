58749bb80e793ace9597bef866c8f16bbb1e9a91
Pablo <pablomurbaez@yahoo.com>
2023-12-24 23:41:06 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  let price;
+
+  if (someValue <= 400) {
+    price = "This one is on me!"; 
+  } else if (someValue <= 2000) {
+    price = 'That will be twenty bucks.'; 
+  } else if (someValue <= 2500) {
+    price = 'I will gladly take your thirty bucks.';
+  } else {
+    return "No can do.";
+  }
+
+  return price;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
